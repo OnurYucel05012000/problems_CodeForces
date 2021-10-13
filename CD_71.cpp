89b161ea66aad0fb107883f55cb822b44b5961cb
OnurYucel2000 <39805114+OnurYucel05012000@users.noreply.github.com>
2021-10-13 16:40:41 +0300

    Update CD_71.cpp

--- CD_71.cpp
+++ CD_71.cpp
@@ -6,18 +6,20 @@ int main() {
 
 	std::string STR;
 	int T;
+	std::cout<<"Enter T"<<"\n";
 	std::cin >> T;
 	
 	while (T--) {
+		std::cout<<"Enter word"<<"\n";
 		std::cin >> STR;
 		if (STR.length() > 10) {
 
 			int nChar = STR.length() - 2;
-			std::string Strnchar = std::to_string(nChar);
+			
 			std::string Newstr;
 
 
-			Newstr = STR.substr(0, 1) + Strnchar + STR.substr(STR.length() - 1, 1);
+			Newstr = STR.substr(0, 1) + std::to_string(nChar) + STR.substr(STR.length() - 1, 1);
 
 			std::cout << Newstr << "\n";
 		}
